2408d3c7e1d442dcf481e190245c532d2d6c467f
sajibsaifullah <dusajibphy@gmail.com>
2023-05-13 00:22:17 +0600

    jwt token

---
+++ __note__.js
@@ -0,0 +1,16 @@
+/**
+ *  ==> JWT -- JSON WEB TOKEN
+ * 
+ *  - Access token
+ *  - Refresh token
+ * 
+ * 
+ *  => local storage -- not secure
+ *  => HTTP only cookie -- partially secure
+ * 
+ * 
+ * Generate token:
+ * ==> in powershell -> require('crypto').randomBytes(64).toString('hex')
+ * 
+ * 
+*/
