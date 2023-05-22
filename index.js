c3e9b98419459509c57afd1aeb7a71e4b06ad3d1
sajibsaifullah <dusajibphy@gmail.com>
2023-05-22 16:34:54 +0600

    search & sort

--- index.js
+++ index.js
@@ -22,22 +22,26 @@ const client = new MongoClient(uri, {
 });
 
 const verifyJWT = (req, res, next) => {
-  console.log('hitting verify JWT');
+  console.log("hitting verify JWT");
   console.log(req.headers.authorization);
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error: true, message: 'unauthorized access'})
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
-  const token = authorization.split(' ')[1];
-  console.log('token inside Verify JWT', token);
+  const token = authorization.split(" ")[1];
+  console.log("token inside Verify JWT", token);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-    if(error){
-      return res.status(403).send({error: true, message: 'unauthorized access'})
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
+  });
+};
 
 async function run() {
   try {
@@ -52,15 +56,28 @@ async function run() {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1hr',
+        expiresIn: "1hr",
       });
-      console.log(token)
-      res.send({token});
+      console.log(token);
+      res.send({ token });
     });
 
-    // services routes
+    // services routes & sorting price & search
     app.get("/services", async (req, res) => {
-      const cursor = serviceCollection.find();
+      const sort = req.query.sort;
+      const search = req.query.search;
+
+      // const query = {};
+      // const query = { price: { $gt: 50, $lte: 150 } };
+      // db.users.find( { name : { $regex : yourvalue, $options : 'i' } } )
+
+      const query = { title: { $regex: search, $options: 'i' } };
+      const options = {
+        // sort matched documents in descending order by rating
+        sort: { price: sort === "asc" ? 1 : -1 },
+      };
+
+      const cursor = serviceCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -80,12 +97,11 @@ async function run() {
 
     // bookings routes
     app.get("/bookings", verifyJWT, async (req, res) => {
-
       const decoded = req.decoded;
-      console.log('came back after verify', decoded)
+      console.log("came back after verify", decoded);
 
-      if(decoded.email !== req.query.email){
-        return res.status(403).send({error: 1, message: 'forbidden access'})
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "forbidden access" });
       }
 
       let query = {};
